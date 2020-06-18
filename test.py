13fc88cbd397b2a07a8a3a5c4273344ed0ce7d3e
styler00dollar <51405565+styler00dollar@users.noreply.github.com>
2020-06-18 10:13:00 +0200

    adding skip parameter

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ device = torch.device('cuda')  # if you want to run on CPU, change 'cuda' -> cpu
 # device = torch.device('cpu')
 
 test_img_folder = 'LR/*'
+results_folder = 'results/*'
 
 model = arch.RRDB_Net(3, 3, 64, 23, gc=32, upscale=int(sys.argv[2]), norm_type=None, act_type='leakyrelu', \
                         mode='CNA', res_scale=1, upsample_mode='upconv')
@@ -23,7 +24,31 @@ model = model.to(device)
 print('Model path {:s}. \nTesting...'.format(model_path))
 
 idx = 0
+
 for path in glob.glob(test_img_folder):
+  skip = 0
+  for result_images in glob.glob(results_folder):
+    if os.path.splitext(os.path.basename(result_images))[0][0:-4] == os.path.splitext(os.path.basename(path))[0]:
+      skip = 1
+
+  #print(skip)
+
+  if skip == 0 and int(sys.argv[3]) == 0:
+    idx += 1
+    base = os.path.splitext(os.path.basename(path))[0]
+    print(idx, base)
+    # read image
+    img = cv2.imread(path, cv2.IMREAD_COLOR)
+    img = img * 1.0 / 255
+    img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+    img_LR = img.unsqueeze(0)
+    img_LR = img_LR.to(device)
+
+    output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+    output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
+    output = (output * 255.0).round()
+    cv2.imwrite('results/{:s}_rlt.png'.format(base), output)
+  if int(sys.argv[3]) == 1:
     idx += 1
     base = os.path.splitext(os.path.basename(path))[0]
     print(idx, base)
